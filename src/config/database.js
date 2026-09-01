67ea1df9bc437c53b369cf4a503f8486f485597e
anahijsb04-ai <anahijsb04@gmail.com>
2026-09-01 12:27:35 -0500

    Primer commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,13 @@
 const { Pool } = require('pg');
 
+console.log('==============================');
+console.log('CONFIGURACIÓN POSTGRESQL');
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('==============================');
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
